e4a686e2aa309f9d4e5b8c6aa0fe6292d4bba5c9
nsan1129 <nsan1129@gmail.com>
2016-03-17 10:30:56 -0700

    Anchor 'Errors' label to bottom.

--- NeverClicker/Forms/MainForm.Designer.cs
+++ NeverClicker/Forms/MainForm.Designer.cs
@@ -238,7 +238,7 @@
 			// 
 			// labelErrors
 			// 
-			this.labelErrors.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			this.labelErrors.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.labelErrors.AutoSize = true;
 			this.labelErrors.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
